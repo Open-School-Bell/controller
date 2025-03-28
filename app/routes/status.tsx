0c08ff7aa609145ee19f448424a3ba26dca9b3d6
Adam Laycock <adam@alaycock.co.uk>
2025-03-28 21:21:02 +0000

    feat: basic status

---
+++ app/routes/status.tsx
@@ -0,0 +1,23 @@
+import {type LoaderFunctionArgs} from '@remix-run/node'
+
+import {getPrisma} from '~/lib/prisma.server'
+import {VERSION} from '~/lib/constants'
+import {getSettings} from '~/lib/settings.server'
+
+export const loader = async ({}: LoaderFunctionArgs) => {
+  const prisma = getPrisma()
+
+  const {lockdownMode} = await getSettings(['lockdownMode'])
+
+  const sounders = await prisma.sounder.findMany({
+    select: {id: true, name: true, lastCheckIn: true, enrolled: true, ip: true}
+  })
+  const zones = await prisma.zone.findMany()
+
+  return Response.json({
+    lockdown: lockdownMode === '1',
+    sounders,
+    version: VERSION,
+    zones
+  })
+}
