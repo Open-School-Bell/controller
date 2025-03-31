1e9ca63a2a35d690ea00ed77ab0f039d40b5b702
Adam Laycock <adam@alaycock.co.uk>
2025-03-31 09:41:29 +0100

    feat: make sounders status an object

--- app/routes/status.tsx
+++ app/routes/status.tsx
@@ -4,6 +4,9 @@ import {getPrisma} from '~/lib/prisma.server'
 import {VERSION} from '~/lib/constants'
 import {getSettings} from '~/lib/settings.server'
 
+type ArrayElement<ArrayType extends readonly unknown[]> =
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never
+
 export const loader = async ({}: LoaderFunctionArgs) => {
   const prisma = getPrisma()
 
@@ -14,9 +17,15 @@ export const loader = async ({}: LoaderFunctionArgs) => {
   })
   const zones = await prisma.zone.findMany()
 
+  const soundersObject: {[sounderId: string]: ArrayElement<typeof sounders>} =
+    {}
+  sounders.forEach(sounder => {
+    soundersObject[sounder.id] = sounder
+  })
+
   return Response.json({
     lockdown: lockdownMode === '1',
-    sounders,
+    sounders: soundersObject,
     version: VERSION,
     zones
   })
