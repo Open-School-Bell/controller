921a8876a3eb8689db004eb096d9615ac36a362e
Adam Laycock <adam@alaycock.co.uk>
2025-04-03 11:04:30 +0100

    feat: version testing on about page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -5,6 +5,7 @@ import {
 } from '@remix-run/node'
 import {useLoaderData} from '@remix-run/react'
 import {asyncForEach} from '@arcath/utils'
+import semver from 'semver'
 
 import {pageTitle} from '~/lib/utils'
 import {Page} from '~/lib/ui'
@@ -29,9 +30,66 @@ export const loader = async ({request}: LoaderFunctionArgs) => {
         response
           .json()
           .then(data => resolve(data))
-          .catch(() => resolve({version: 'error', piperVersion: 'error'}))
+          .catch(() => resolve({version: '0.0.0', piperVersion: '0.0.0'}))
       })
-      .catch(() => resolve({version: 'error', piperVersion: 'error'}))
+      .catch(() => resolve({version: '0.0.0', piperVersion: '0.0.0'}))
+  })
+
+  const sounderLatest = await new Promise<string>(resolve => {
+    fetch(
+      'https://api.github.com/repos/Open-School-Bell/sounder/releases?per_page=1',
+      {
+        headers: {
+          Accept: 'application/vnd.github+json',
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      }
+    )
+      .then(response => {
+        response
+          .json()
+          .then(data => resolve(data[0].tag_name))
+          .catch(() => resolve('error'))
+      })
+      .catch(() => resolve('error'))
+  })
+
+  const ttsLatest = await new Promise<string>(resolve => {
+    fetch(
+      'https://api.github.com/repos/Open-School-Bell/tts/releases?per_page=1',
+      {
+        headers: {
+          Accept: 'application/vnd.github+json',
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      }
+    )
+      .then(response => {
+        response
+          .json()
+          .then(data => resolve(data[0].tag_name))
+          .catch(() => resolve('error'))
+      })
+      .catch(() => resolve('error'))
+  })
+
+  const controllerLatest = await new Promise<string>(resolve => {
+    fetch(
+      'https://api.github.com/repos/Open-School-Bell/controller/releases?per_page=1',
+      {
+        headers: {
+          Accept: 'application/vnd.github+json',
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      }
+    )
+      .then(response => {
+        response
+          .json()
+          .then(data => resolve(data[0].tag_name))
+          .catch(() => resolve('error'))
+      })
+      .catch(() => resolve('error'))
   })
 
   const prisma = getPrisma()
@@ -50,7 +108,14 @@ export const loader = async ({request}: LoaderFunctionArgs) => {
     sounderVersions[id] = version ? version : '0.0.0'
   })
 
-  return {piperData, sounders, sounderVersions}
+  return {
+    piperData,
+    sounders,
+    sounderVersions,
+    sounderLatest,
+    ttsLatest,
+    controllerLatest
+  }
 }
 
 export const meta: MetaFunction = () => {
@@ -58,7 +123,14 @@ export const meta: MetaFunction = () => {
 }
 
 const About = () => {
-  const {piperData, sounders, sounderVersions} = useLoaderData<typeof loader>()
+  const {
+    piperData,
+    sounders,
+    sounderVersions,
+    sounderLatest,
+    ttsLatest,
+    controllerLatest
+  } = useLoaderData<typeof loader>()
 
   return (
     <Page title="About">
@@ -75,28 +147,56 @@ const About = () => {
           <tr>
             <td>openschoolbell/controller</td>
             <td className="text-center">{VERSION}</td>
-            <td></td>
-            <td>{RequiredVersions.controller}</td>
+            <td
+              className={`text-center ${semver.gt(controllerLatest, VERSION) ? 'bg-red-300' : ''}`}
+            >
+              {controllerLatest.replace('v', '')}
+            </td>
+            <td className="text-center">{RequiredVersions.controller}</td>
           </tr>
           <tr>
             <td>openschoolbell/tts</td>
             <td className="text-center">{piperData.version}</td>
-            <td></td>
-            <td>{RequiredVersions.tts}</td>
+            <td
+              className={`text-center ${semver.gt(ttsLatest, piperData.version) ? 'bg-red-300' : ''}`}
+            >
+              {ttsLatest.replace('v', '')}
+            </td>
+            <td
+              className={`text-center ${semver.gt(RequiredVersions.tts, piperData.version) ? 'bg-red-300' : ''}`}
+            >
+              {RequiredVersions.tts}
+            </td>
           </tr>
           <tr>
             <td>piper</td>
             <td className="text-center">{piperData.piperVersion}</td>
-            <td></td>
-            <td>{RequiredVersions.piper}</td>
+            <td
+              className={`text-center ${semver.gt(piperData.piperVersion, piperData.version) ? 'bg-red-300' : ''}`}
+            >
+              {piperData.piperVersion}
+            </td>
+            <td
+              className={`text-center ${semver.gt(RequiredVersions.piper, piperData.version) ? 'bg-red-300' : ''}`}
+            >
+              {RequiredVersions.piper}
+            </td>
           </tr>
           {sounders.map(({id, name}) => {
             return (
               <tr key={id}>
                 <td>Sounder: {name}</td>
                 <td className="text-center">{sounderVersions[id]}</td>
-                <td></td>
-                <td>{RequiredVersions.sounder}</td>
+                <td
+                  className={`text-center ${semver.gt(sounderLatest, sounderVersions[id]) ? 'bg-red-300' : ''}`}
+                >
+                  {sounderLatest.replace('v', '')}
+                </td>
+                <td
+                  className={`text-center ${semver.gt(RequiredVersions.sounder, sounderVersions[id]) ? 'bg-red-300' : ''}`}
+                >
+                  {RequiredVersions.sounder}
+                </td>
               </tr>
             )
           })}
