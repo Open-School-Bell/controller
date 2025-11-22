c39a339ac6749712b6d0c57f56ad39d2c86c58f6
Adam Laycock <adam@alaycock.co.uk>
2025-11-22 21:22:32 +0000

    feat: button trigger

---
+++ app/routes/button-api.trigger.tsx
@@ -0,0 +1,47 @@
+import {type ActionFunctionArgs} from '@remix-run/node'
+
+import {getPrisma} from '~/lib/prisma.server'
+import {broadcast} from '~/lib/broadcast.server'
+import {toggleLockdown} from '~/lib/lockdown.server'
+
+export const action = async ({request}: ActionFunctionArgs) => {
+  const {key, zone} = (await request.json()) as {
+    key?: string
+    zone?: string
+  }
+
+  if (!key || typeof key !== 'string') {
+    return Response.json({error: 'missing key'}, {status: 400})
+  }
+
+  const prisma = getPrisma()
+
+  const button = await prisma.actionButton.findFirst({
+    where: {key, enrolled: true},
+    include: {action: true}
+  })
+
+  if (!button) {
+    return Response.json({error: 'sounder not found'}, {status: 401})
+  }
+
+  switch (button.action.action) {
+    case 'broadcast':
+      if (!zone || typeof zone !== 'string' || zone.trim() === '') {
+        return Response.json({error: 'missing zone'}, {status: 400})
+      }
+
+      if (button.action.audioId) {
+        const zoneId = zone.trim()
+        await broadcast(zoneId, JSON.stringify([button.action.audioId]))
+      }
+      break
+    case 'lockdown':
+      await toggleLockdown()
+      break
+    default:
+      break
+  }
+
+  return Response.json({ping: 'pong'})
+}
